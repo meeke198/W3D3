86858b6edbd2530221d5a2c390bc8157d78e2ac3
hien nguyen <thuhiennguyen198@gmail.com>
2021-08-25 16:10:42 -0400

    bsearch

--- recursion.rb
+++ recursion.rb
@@ -108,14 +108,24 @@ def fib(n)
 end
 # p fib(5)
 
-def b_search
+def bsearch(arr, target)
+    return nil if arr.empty?
+    pivot = arr.length / 2
+    if (target <=> arr[pivot]) == -1
+        bsearch(arr.take(pivot), target)
+    elsif (target <=> arr[pivot]) == 0
+        return pivot
+    end
+        bsearch(arr.drop(pivot), target)
+        
+    end
     
 end
 
-bsearch([1, 2, 3], 1) # => 0
-bsearch([2, 3, 4, 5], 3) # => 1
-bsearch([2, 4, 6, 8, 10], 6) # => 2
-bsearch([1, 3, 4, 5, 9], 5) # => 3
-bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
-bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
-bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+p bsearch([1, 2, 3], 1) # => 0
+p bsearch([2, 3, 4, 5], 3) # => 1
+p bsearch([2, 4, 6, 8, 10], 6) # => 2
+p bsearch([1, 3, 4, 5, 9], 5) # => 3
+p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
