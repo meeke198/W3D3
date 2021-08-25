1cbc819bb33e885a71eb6e8b24891b9450496c79
hien nguyen <thuhiennguyen198@gmail.com>
2021-08-25 14:04:12 -0400

    First recursions

--- recursion.rb
+++ recursion.rb
@@ -7,12 +7,38 @@ def range(start_num, end_num)
 end
 
 def sum_arr(arr)
-    
+    sum = 0
+    arr.each {|ele| sum += ele}
+    sum
+   
 end
+# p sum_arr([1, 2, 3])
+def sum_arr_re(arr)
+  return arr.first if arr.length == 1
+  return 0 if arr.length == 0
+  arr.shift + sum_arr_re(arr)
+end
+# p sum_arr_re([1, 2, 3])
+# p sum_arr_re([1, 2, 3, 9, 0])
+# p sum_arr_re([])
 
 #exponentiation
 #v1
-
+def exp_1(base, exp)
+    return 1 if exp == 0
+    base * exp_1(base, exp - 1)
+end
+# p exp_1(2, 3)
 #v2
-
+def exp_2(base, exp)
+    return 1 if exp == 0
+    return base if exp == 1
+    if exp.even?
+        exp_2(base, exp / 2) ** 2 
+    else
+       base * (exp_2(base, (exp - 1) / 2) ** 2) 
+    end
+end
+p exp_2(2,8)
+p exp_2(2,7)
 #deep dup
